ddfdf28c76d4eeb7ab66c5f51eea5fa9db2ebeb9
nabsty55 <nabsty55@gmail.com>
2019-10-08 13:24:15 +0300

    added bcrypt to has password

--- routes/auth.js
+++ routes/auth.js
@@ -1,20 +1,25 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
 
 
 
 router.post('/register', async (req, res)=>{
 
-//check is email exists
+//check if email already exists
   const emailExists = await User.findOne({email: req.body.email});
   if(emailExists) return res.status(400).send("Email already exists");
+
+  //Hashing the password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
 
 
   const user = new User ({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password
+    password: hashedPassword
   });
   try {
 
